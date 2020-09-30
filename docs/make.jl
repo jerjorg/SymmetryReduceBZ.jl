c71cd7548755013cc5295dcb321315403261c017
Jeremy Jorgensen <jerjorg@gmail.com>
2020-09-29 20:03:32 -0600

    Fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -5,8 +5,10 @@ makedocs(sitename="IBZ",
          # format = Documenter.HTML(prettyurls = false),
          modules = [IBZ],
          authors = "Jeremy Jorgensen",
+         doctest = true,
          pages=["index.md", "Documentation.md", "Usage.md"])
 
 deploydocs(
     repo = "github.com/jerjorg/IBZ.jl.git",
-    versions = ["stable" => "v#.#"])
+    versions = ["stable" => "v#.#"],
+    devurl = "docs")
