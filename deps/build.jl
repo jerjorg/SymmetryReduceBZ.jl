88095418190320380f1a4c683b4fffe2c614d65f
Jeremy Jorgensen <jerjorg@gmail.com>
2021-01-04 14:21:22 -0700

    Added deps/build.jl to install sympy

---
+++ deps/build.jl
@@ -0,0 +1,28 @@
+using PyCall
+
+println("Running build.jl for the ComputeIBZ package.")
+
+# Change that to whatever packages you need.
+const PACKAGES = ["sympy"]
+
+# Use eventual proxy info
+proxy_arg=String[]
+if haskey(ENV, "http_proxy")
+    push!(proxy_arg, "--proxy")
+    push!(proxy_arg, ENV["http_proxy"])
+end
+
+# Import pip
+try
+    pyimport("pip")
+catch
+    # If it is not found, install it
+    println("Pip not found on your sytstem. Downloading it.")
+    get_pip = joinpath(dirname(@__FILE__), "get-pip.py")
+    download("https://bootstrap.pypa.io/get-pip.py", get_pip)
+    run(`$(PyCall.python) $(proxy_arg) $get_pip --user`)
+end
+
+println("Installing required python packages using pip")
+run(`$(PyCall.python) $(proxy_arg) -m pip install --user --upgrade pip setuptools`)
+run(`$(PyCall.python) $(proxy_arg) -m pip install --user $(PACKAGES)`)
