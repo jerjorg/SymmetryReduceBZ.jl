adf67c176a86cbf427ba46398593a5ce79812412
lxvm <lorenzo@vanmunoz.com>
2023-12-29 22:44:57 -0800

    rollback support to 1.6

--- src/SymmetryReduceBZ.jl
+++ src/SymmetryReduceBZ.jl
@@ -10,4 +10,9 @@ import .Symmetry: calc_bz, calc_ibz
 
 export calc_bz, calc_ibz, plot_convexhulls
 
+if !isdefined(Base, :get_extension)
+    include("../ext/SymmetryReduceBZPyPlotExt.jl")
+    include("../ext/SymmetryReduceBZQHullExt.jl")
+end
+
 end #module
